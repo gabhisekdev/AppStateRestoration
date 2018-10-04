fae922117bc80a3bac42e0a365b7489ab7f18b2d
G Abhisek <abhisek.g@olacabs.com>
2018-10-04 14:38:52 +0530

    AppRestoration first sample commit

---
+++ AppStateRestoration1/AppStateRestoration1/ComicWorldController.swift
@@ -0,0 +1,21 @@
+//
+//  ComicWorldController.swift
+//  AppRestorationExperiments
+//
+//  Created by G Abhisek on 28/08/18.
+//  Copyright © 2018 G Abhisek. All rights reserved.
+//
+
+import UIKit
+
+class ComicWorldController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
---
+++ AppStateRestoration1/AppStateRestoration1/PhotosViewController.swift
@@ -0,0 +1,52 @@
+//
+//  PhotosViewController.swift
+//  AppRestorationExperiments
+//
+//  Created by G Abhisek on 28/08/18.
+//  Copyright © 2018 G Abhisek. All rights reserved.
+//
+
+import UIKit
+
+enum SuperHeroType: Int {
+    case batman = 0
+    case ironman
+}
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet weak var superHeroImageView: UIImageView!
+    
+    var selectedSuperheroId: SuperHeroType = .ironman
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setImage()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func switchItems(_ sender: Any) {
+        selectedSuperheroId = selectedSuperheroId == .ironman ? .batman : .ironman
+        setImage()
+    }
+    
+    private func setImage() {
+        superHeroImageView.image = selectedSuperheroId == .ironman ? #imageLiteral(resourceName: "ironman") : #imageLiteral(resourceName: "batman")
+    }
+   
+}
+
+extension PhotosViewController {
+    override func encodeRestorableState(with coder: NSCoder) {
+        coder.encode(selectedSuperheroId.rawValue, forKey: "imageId")
+        super.encodeRestorableState(with: coder)
+    }
+
+    override func decodeRestorableState(with coder: NSCoder) {
+        selectedSuperheroId = SuperHeroType(rawValue: coder.decodeInteger(forKey: "imageId")) ?? .batman
+        setImage()
+    }
+}
